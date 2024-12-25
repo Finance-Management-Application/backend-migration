b41e244576a6d79b4926becafd202ea4a40ee0ed
Amit Kumar Mahapatra <mahapatraarhas@gmail.com>
2024-12-25 21:10:56 +0530

    Test commit

---
+++ src/main.py
@@ -0,0 +1,1 @@
+from services.models import expense
---
+++ src/services/reader.py
@@ -0,0 +1,22 @@
+import logging
+
+from openpyxl import load_workbook
+
+from ..core.constants import Workbook 
+from ..core.constants import WORKBOOK_SHEET_PYDANTIC_MODEL_MAPPING as mapping
+
+def read(sheet:Workbook):
+    wb = load_workbook(filename=Workbook.LOCATION, read_only=True, data_only=True)
+    ws = wb[sheet]
+    
+    columns = [cell.value for cell in next(ws.rows)]
+    keys = ["_".join(column.lower().split()) for column in columns]
+
+    data = []
+    for row in ws.iter_rows(min_row=2):
+        record = {key: cell.value for key, cell in zip(keys, row)}
+        model = mapping.get(sheet)
+        if model:
+            data.append(model(**record).model_dump())
+    
+    return data
