135fef043f16e4191ad1fe4df8d4dcf917d0d43c
Amit Global <mahapatraarhas200@gmail.com>
2024-12-28 07:02:15 +0530

    Implemented Exception Handling and started adding the success and failure percentage in the readers function

--- src/main.py
+++ src/main.py
@@ -6,8 +6,9 @@ sys.path.append(str(Path(__file__).resolve().parent.parent.joinpath('log')))
 from constants.const import Expense
 from services.reader import read, reads
 
-# categories = read(**Expense.CATEGORY.value)
+# categories, failure = read(**Expense.CATEGORY.value)
 # sub_categories = read(**Expense.SUB_CATEGORY.value)
-transactions = reads(**Expense.TRANSACTION.value)
+transactions, failure = reads(**Expense.TRANSACTION.value)
 
 print(transactions)
+print(failure)
--- src/services/reader.py
+++ src/services/reader.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Tuple, Union
 from pathlib import Path
 
 from pydantic import BaseModel
@@ -7,70 +7,90 @@ from openpyxl import load_workbook
 from openpyxl.utils.exceptions import InvalidFileException
 
 from logging import DEBUG
-from logconfig import get_logger
+from logconfig import get_logger # type: ignore
 
-logger = get_logger(__name__, __file__, DEBUG)
+logger = get_logger(__name__, f'{__file__[:-3]}.log', DEBUG)
 
 
-def read(workbook: Path, max_col: int, model: BaseModel) -> List[dict]:  
-    """
-    Read and validate data from a single Excel workbook.
+def read(workbook: Path, max_col: int, model: BaseModel) -> Tuple[List[dict], int]:  
+    if not isinstance(workbook, Path):
+        raise TypeError(f'FunctionParameterError: {workbook} must be a {Path} type Object')
+    if not isinstance(max_col, int):
+        raise TypeError(f'FunctionParameterError: {max_col} must be a {int} type Object')
+    if not issubclass(model, BaseModel):
+        raise TypeError(f'FunctionParameterError: {model} must be a subclass of {BaseModel} type Object')
+
+
+    try:
+        wb = load_workbook(filename=workbook, read_only=True, data_only=True)
+    except FileNotFoundError as e:
+        logger.error(f'WorkbookOpeningError: File not found in {workbook} Path')
+        raise
+    except InvalidFileException as e:
+        logger.error(f'WorkbookOpeningError: {workbook.name} seems to be a non-ooxml(not xlsx format in this case) file')
+        raise
+    except Exception as e:
+        logger.error(f'WorkbookOpeningError: Unknown Error in opening {workbook.name}')
+        raise
     
-    Args:
-        workbook: Path to Excel workbook
-        columns: Number of columns to read
-        model: Pydantic model for validation
-        
-    Returns:
-        List of validated data dictionaries
-    """    
-    # Open Workbook
-    # Multiple issue can happen while opening workbook
-    # 1. Caller of the function passes wrong parameter
-    # 2. File does not exists
-    # 3. Valid file does not exists, incorrect format or something
-    # 4. File exists and valid, but corrupted, so could not open
-    wb = load_workbook(filename=workbook, read_only=True, data_only=True)
-    
-    
-    # Open Worksheet
-    # wb.active => is by default set to 0 by openpyxl, and always provides the first sheet
-    # So no chance of exception here
+
+    # wb.active => default set to 0 by openpyxl, and always provides the first sheet
     ws = wb.active
 
-    # max_col, even though its more than actual columns, still this will not raise exception
-    # but StopIteration may get raised if the excel sheet is empty
-    columns = [cell.value for cell in next(ws.iter_rows(max_col=max_col))]
+    try:
+        columns = [cell.value for cell in next(ws.iter_rows(max_col=max_col))]
+    except StopIteration as e:
+        logger.error(f'Sheet {str(ws)} is Empty')
+        raise
+
     # If max_col value is given wrong, then lower() will be called on a None object which will be an exception
-    # I need the stacktrace here
-    keys = ["_".join(column.lower().split()) for column in columns]
+    try:
+        keys = ["_".join(column.lower().split()) for column in columns]
+    except AttributeError as e:
+        logger.error(f'max_col={max_col} parameter value is higher than expected')
+        raise
 
     data = []
     for n, row in enumerate(ws.iter_rows(min_row=2, max_col=max_col), start=2):
-        record = {key: cell.value for key, cell in zip(keys, row)}
+        # record = {key: cell.value for key, cell in zip(keys, row)}
 
         record = {}
         for key, cell in zip(keys, row):
-            # If number of col headers < number of columns in the row => key will be empty and record[key] => exception
-            record[key] = cell.value
+            # case where there are more row cells than headers
+            try:
+                record[key] = cell.value
+            except KeyError as e:
+                logger.error(f"Row {n} has more columns than headers")
+                raise
 
         # Get Data Row by Row
         # Issues may come with data, which Pydantic is going to handle
         # So use the exception classes given by pydantic library
         try:
             data.append(model(**record).model_dump())
-        except Exception as e:
-            logger.debug(f'EXCEL FILE: {workbook.stem} ROW: {n} \n Issue: {e}')
+        except ValidationError as e:
+            logger.debug(f'DataError: {workbook.name} Row: {n} \n {e}')
 
     return data
 
 
-def reads(workbooks: List[Path], columns: int, model: BaseModel) -> List[dict]:
-    """Read and validate data from multiple Excel workbooks."""
-    data = []
+def reads(workbooks: List[Path], max_col: int, model: BaseModel) -> Tuple[List[dict], int]:
+    """Read and validate data from multiple Uniform Excel workbooks, i.e. max_col of each must be same."""
 
-    # caller might send a non iterable object
+    if isinstance(workbooks, list):
+        raise TypeError(f'FunctionParameterError: {workbooks} must be a {list} type Object')
+    if not isinstance(max_col, int):
+        raise TypeError(f'FunctionParameterError: {max_col} must be a {int} type Object')
+    if not issubclass(model, BaseModel):
+        raise TypeError(f'FunctionParameterError: {model} must be a subclass of {BaseModel} type Object')
+
+    data, fail_count, success_count = [], 0, 0
     for workbook in workbooks:
         # issue in read function
-        data.extend(read(workbook, columns, model))
-    return data
+        try:
+            data.extend(read(workbook, max_col, model))
+            success_count +=1
+        except Exception as e:
+            fail_count +=1
+            continue
+    return data, round(success_count/(success_count+fail_count)*100)
